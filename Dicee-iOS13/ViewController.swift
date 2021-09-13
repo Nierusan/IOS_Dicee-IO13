aff8ebe1d81655acfe987386e3a1dc47d646ebd3
Yvan <ythevoz@gmail.com>
2021-09-13 11:54:12 +0200

    Variable and loop challenge

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,19 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        // WHO.WHAT = Value
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
-       /* diceImageViewOne.alpha = 0.5 */
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+ 
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageViewOne.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
+        diceImageViewTwo.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][rightDiceNumber]
 
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
     }
     
 }
